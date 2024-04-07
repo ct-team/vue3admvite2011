6cceefcc9bf2074fc0610bc93a473a6999bbf819
daixl <daixl@ct108.com>
2024-04-07 19:39:54 +0800

    ...

--- template/src/views/index/Search.vue
+++ template/src/views/index/Search.vue
@@ -83,7 +83,7 @@ const setLoading = (flag: boolean): void => {
   loading.value = flag;
 };
 const search = () => {
-  emit('search', JSON.parse(JSON.stringify(formData)));
+  emit('search', { ...formData });
 };
 defineExpose({
   setLoading,
