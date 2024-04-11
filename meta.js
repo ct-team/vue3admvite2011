4161c88a20253e3103ba267eb50d796ca83215ba
daixl <daixl@ct108.com>
2024-04-11 13:33:04 +0800

    1.1.1

--- meta.js
+++ meta.js
@@ -1,37 +1,37 @@
-const path = require('path')
-const fs = require('fs')
+const path = require('path');
+const fs = require('fs');
 
 const {
   sortDependencies,
   installDependencies,
   runLintFix,
   printMessage,
-} = require('./utils')
-const pkg = require('./package.json')
+} = require('./utils');
+const pkg = require('./package.json');
 
-const templateVersion = pkg.version
+const templateVersion = pkg.version;
 
-const { addTestAnswers } = require('./scenarios')
+const { addTestAnswers } = require('./scenarios');
 
 module.exports = {
   metalsmith: {
     // When running tests for the template, this adds answers for the selected scenario
-    before: addTestAnswers
+    before: addTestAnswers,
   },
+  skipInterpolation: ['src/**/*.vue', 'node_modules/**'],
   helpers: {
     if_or(v1, v2, options) {
-
       if (v1 || v2) {
-        return options.fn(this)
+        return options.fn(this);
       }
 
-      return options.inverse(this)
+      return options.inverse(this);
     },
     template_version() {
-      return templateVersion
+      return templateVersion;
     },
   },
-  
+
   prompts: {
     name: {
       when: 'isNotTest',
@@ -50,31 +50,29 @@ module.exports = {
       when: 'isNotTest',
       type: 'string',
       message: 'Author',
-    }
+    },
   },
-  filters: {
+  filters: {},
+  complete: function (data, { chalk }) {
+    const green = chalk.green;
 
-  },
-  complete: function(data, { chalk }) {
-    const green = chalk.green
+    sortDependencies(data, green);
 
-    sortDependencies(data, green)
-
-    const cwd = path.join(process.cwd(), data.inPlace ? '' : data.destDirName)
+    const cwd = path.join(process.cwd(), data.inPlace ? '' : data.destDirName);
 
     if (data.autoInstall) {
       installDependencies(cwd, data.autoInstall, green)
         .then(() => {
-          return runLintFix(cwd, data, green)
+          return runLintFix(cwd, data, green);
         })
         .then(() => {
-          printMessage(data, green)
+          printMessage(data, green);
         })
-        .catch(e => {
-          console.log(chalk.red('Error:'), e)
-        })
+        .catch((e) => {
+          console.log(chalk.red('Error:'), e);
+        });
     } else {
-      printMessage(data, chalk)
+      printMessage(data, chalk);
     }
   },
-}
+};
